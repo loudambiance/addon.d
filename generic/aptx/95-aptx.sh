a1820eecc99f17e35c2c781a3691bee57b307497
Dan Pasanen <dan.pasanen@gmail.com>
2017-02-27 07:39:35 -0600

    add libaptx backup script

---
+++ generic/aptx/95-aptx.sh
@@ -0,0 +1,40 @@
+#!/sbin/sh
+#
+# /system/addon.d/95-aptx.sh
+#
+
+. /tmp/backuptool.functions
+
+list_files() {
+cat <<EOF
+lib/libaptX-1.0.0-rel-Android21-ARMv7A.so
+lib/libaptXScheduler.so
+EOF
+}
+
+case "$1" in
+  backup)
+    list_files | while read FILE DUMMY; do
+      backup_file $S/$FILE
+    done
+  ;;
+  restore)
+    list_files | while read FILE REPLACEMENT; do
+      R=""
+      [ -n "$REPLACEMENT" ] && R="$S/$REPLACEMENT"
+      [ -f "$C/$S/$FILE" ] && restore_file $S/$FILE $R
+    done
+  ;;
+  pre-backup)
+    # Stub
+  ;;
+  post-backup)
+    # Stub
+  ;;
+  pre-restore)
+    # Stub
+  ;;
+  post-restore)
+    # Stub
+  ;;
+esac
---
+++ generic/aptx/README.md
@@ -0,0 +1,7 @@
+AptX libs
+================
+
+Addon.d script for to backup libaptX libs and restore it during updates.
+
+I am in no way responsible for any damages to your device that using this script
+may cause.
